6be69eb6601c9033e9568fd20cb6b6c29e1a9a19
Daniel <dds.daniel@gmail.com>
2020-05-03 08:47:46 -0300

    criando projeto + primeiras classes

---
+++ src/DDS.WebAPI/Abstractions/Controllers/CrudController.cs
@@ -0,0 +1,154 @@
+﻿using AutoMapper;
+using DDS.Domain.Core.Abstractions.Model.Entities;
+using DDS.Domain.Core.Abstractions.Services;
+using DDS.WebAPI.Abstractions.ViewModels;
+using Flunt.Notifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DDS.WebAPI.Abstractions.Controllers
+{
+    /// <summary>
+    /// Controller que provê as implementações genéricas para métodos de CRUD das outras entidades
+    /// </summary>
+    /// <typeparam name="TViewModelCadastro">View model com os dados necessários para o cadastro</typeparam>
+    /// <typeparam name="TViewModelConsulta">View model com os dados que podem ser exibidos no front</typeparam>
+    /// <typeparam name="TEntity">Entidade do domínio</typeparam>
+    public abstract class CrudController<TViewModelCadastro, TViewModelConsulta, TEntity> : GenericController
+        where TViewModelCadastro : IViewModel
+        where TEntity : Entity
+    {
+
+        private readonly ICrudService<TEntity> _crudService;
+        private readonly IMapper _mapper;
+
+
+        /// <summary>
+        /// Construtor com parâmetros passados a partir da base concreta
+        /// </summary>
+        /// <param name="crudService">Serviço para validação e manipulação da entidade</param>
+        /// /// <param name="mapper">Automapper</param>
+        public CrudController(ICrudService<TEntity> crudService,
+                              IMapper mapper)
+        {
+            _crudService = crudService;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Consulta todos os registros do repositório
+        /// </summary>
+        /// <returns>Lista de registros (view model)</returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(ICollection<IViewModel>), StatusCodes.Status200OK)]
+        public virtual ActionResult<ICollection<TViewModelCadastro>> ConsultarTodos()
+        {
+            var lista = _crudService.ConsultarTodos();
+
+            var listaViewModel = _mapper.Map<List<TViewModelConsulta>>(lista);
+
+            return Ok(listaViewModel);
+        }
+
+        /// <summary>
+        /// Consulta um registro no repositório
+        /// </summary>
+        /// <returns>View model de consulta</returns>
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(typeof(IViewModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<Notification>), StatusCodes.Status404NotFound)]
+        public virtual ActionResult<ICollection<TViewModelCadastro>> ConsultarPorId(Guid id)
+        {
+            var entidade = _crudService.ConsultarPorId(id);
+
+            if (entidade == null)
+                return CustomNotFound(nameof(id), "Registro não encontrado");
+
+            var viewModel = _mapper.Map<TViewModelConsulta>(entidade);
+
+            return Ok(viewModel);
+        }
+
+
+        /// <summary>
+        /// Exclui uma entidade do repositório
+        /// </summary>
+        /// <param name="id">Id da entidade que será excluída</param>
+        /// <returns></returns>
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<Notification>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(List<Notification>), StatusCodes.Status400BadRequest)]
+        public virtual async Task<ActionResult> Delete(Guid id)
+        {
+            var entidade = await _crudService.ConsultarPorId(id);
+
+            if (entidade == null)
+                return CustomNotFound(nameof(id), "Registro não encontrado");
+
+            await _crudService.Excluir(id);
+
+            if (_crudService.Invalid)
+                return BadRequest(_crudService.Notifications);
+
+            await _crudService.Commit();
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Cadastra a entidade no repositório
+        /// </summary>
+        /// <param name="novaEntidadeViewModel">Dados da nova entidade</param>
+        /// <returns>Id da entidade recém cadastrada</returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(Guid), StatusCodes.Status200OK)]
+        public virtual async Task<ActionResult<Guid>> Post(TViewModelCadastro novaEntidadeViewModel)
+        {
+            var entidade = _mapper.Map<TEntity>(novaEntidadeViewModel);
+
+            if (entidade.Invalid)
+                return BadRequest(entidade.Notifications);
+
+            await _crudService.Adicionar(entidade);
+
+            if (_crudService.Invalid)
+                return BadRequest(_crudService.Notifications);
+
+            await _crudService.Commit();
+
+            return Ok(new { entidade.Id });
+        }
+
+        /// <summary>
+        /// Altera os dados de uma entidade do repositório
+        /// </summary>
+        /// <param name="id">Id da entidade que será alterada</param>
+        /// <param name="viewModelCadastro">Dados que serão alterados</param>
+        /// <returns>OK (201)</returns>
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<Notification>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(List<Notification>), StatusCodes.Status400BadRequest)]
+        public virtual async Task<ActionResult> Put(Guid id, TViewModelCadastro viewModelCadastro)
+        {
+            var entidade = _mapper.Map<TEntity>(viewModelCadastro);
+
+            if (entidade.Id != id)
+                return CustomBadRequest(nameof(id), "Ids não conferem");
+
+            await _crudService.Atualizar(entidade);
+
+            if (_crudService.Invalid)
+                return BadRequest(_crudService.Notifications);
+
+            await _crudService.Commit();
+
+            return Ok();
+        }
+    }
+}
---
+++ src/DDS.WebAPI/Abstractions/ViewModels/IViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DDS.WebAPI.Abstractions.ViewModels
+{
+    /// <summary>
+    /// Interface a qual todas as view models devem implementar
+    /// </summary>
+    public interface IViewModel
+    {
+    }
+}
